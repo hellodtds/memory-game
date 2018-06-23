1df85a68b011feb3cf1e49f8fc3f2c754a26be16
kaotikus <scott@sudoroom.org>
2018-06-23 17:00:26 -0400

    Chore: Installed `gulp-sass`
    1) Added `gulp-sass` directives to gulp task runner

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,19 @@
+var log = console.log;
+
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+
 
 gulp.task('default', function() {
+
+// First: We need to transpile Sass into CSS
+    gulp.src('sass/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css'));
+
+// Second: We need a tool that adds vendor prefixes, like Auto-prefixer
+    
+    log('this works!');
 
 
 });
